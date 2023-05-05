827f638f042dd81f75aedd019c89630277f38dad
Ken King <kking935@vt.ed>
2023-05-04 23:45:32 -0400

    modified gpt settings

--- app/config/gpt_settings.py
+++ app/config/gpt_settings.py
@@ -2,16 +2,17 @@ MODEL = "gpt-3.5-turbo"
 # MODEL = "gpt-4"
 
 # MAX_TOKENS = 100
-MAX_TOKENS = 200
+MAX_TOKENS = 500
 TEMPERATURE = 0
 
 # INITIAL_SYSTEM_PROMPT = "You are a medical assistant designed to identify social determinants \
 #     of health from patient free response answers to different questions. Here are the 5 \
 #     key domains outlining social determinants of health, as well as their ID, their \
 #     subdomains, and the subdomain IDs- "
-INITIAL_SYSTEM_PROMPT = "You are a medical assistant designed to identify social determinants \
-    of health from patient free response answers to different questions. Here are the 5 \
-    key domains outlining social determinants of health, as well as their their \
+INITIAL_SYSTEM_PROMPT = "You are a medical assistant designed to identify negative social risk factors \
+    related to determinants of health from patient free response answers to different questions. \
+    You only report risk factors that are bad, not risk factors that are good. \
+    Here are the 5 key domains outlining social determinants of health, as well as their their \
     subdomains- "
 
 INITIAL_QA_EXAMPLES = [
@@ -22,7 +23,14 @@ INITIAL_QA_EXAMPLES = [
         ],
         "assistant_response": "Economic Stability: Housing Instability, Poverty. Neighborhood and Built Environment: Environmental Conditions, Quality of Housing."
     },
-        {
+    {
+        "user_prompts": [
+            "Question 2: What is the highest level of education you have acquired?", 
+            "Answer 2: I graduated from Virginia Tech with a bachelor's degree in finance.",
+        ],
+        "assistant_response": ""
+    },
+    {
         "user_prompts": [
             "Summarize the patient's key social risk factors with direct references from their answers.",
         ],
